2a71bfb2a17848a312d852085ccc59b8f783318a
snake <oscar1940@gmail.com>
2020-07-10 19:09:25 -0500

    manejo de variables primitivas numericas

---
+++ HolaMundoJava/src/HolaMundo.java
@@ -0,0 +1,11 @@
+
+/**
+ *
+ * @author snake
+ */
+public class HolaMundo {
+    public static void main(String args[]){
+        System.out.println("Hola Mundo desde Java");
+    }    
+}
+
---
+++ Variables/src/Variables.java
@@ -0,0 +1,29 @@
+
+/**
+ *
+ * @author snake
+ */
+public class Variables {
+
+    public static void main(String[] args) {
+       int miVariableEntera = 10;
+       System.out.println(miVariableEntera);
+       miVariableEntera = 5;
+       System.out.println(miVariableEntera);
+       
+       String miVariableCadena = "Saludos";
+       System.out.println(miVariableCadena);
+       
+       miVariableCadena = "Adios";
+       System.out.println(miVariableCadena);
+       
+       var miVariableEntera2 = 15;
+       System.out.println(miVariableEntera2);
+       
+       var miVariableCadena2 = "Nueva Cadena";
+       System.out.println("miVariableCadena2 = " + miVariableCadena2); 
+
+
+
+    }
+}
