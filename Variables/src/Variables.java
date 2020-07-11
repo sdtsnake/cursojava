9ac8ee867d029e90736d9e4018fe359dc48c4489
snake <oscar1940@gmail.com>
2020-07-11 10:08:15 -0500

    manipulacion casteo de tipos de datos usando var para ver el efecto usar el modo debug para ver como asignan los tipos de datos segun la informacion

--- Variables/src/Variables.java
+++ Variables/src/Variables.java
@@ -8,19 +8,15 @@ import java.util.Scanner;
 public class Variables {
 
     public static void main(String[] args) {
-        /*
-            tipos primitivos flotante : float y double
-         */
-        // se tiene que castear porque 10 es entero
-        float numeroFloat = (float) 10;
-        System.out.println("numeroFloat = " + numeroFloat);
-        System.out.println("valor minimo tipo float:" + Float.MIN_VALUE);
-        System.out.println("valor maximo tipo float:" + Float.MAX_VALUE);
-        // no se castea por tener punto flotante.
-        double numeroDouble = 10.0;
+        var numeroEntero = 10;
+        System.out.println("numeroEntero = " + numeroEntero);
+        
+        var numeroDouble = 10.0;
         System.out.println("numeroDouble = " + numeroDouble);
-        System.out.println("valor minimo tipo double:" + Double.MIN_VALUE);
-        System.out.println("valor maximo tipo double:" + Double.MAX_VALUE);
+        
+        var numeroFloat = 10.0F;
+        System.out.println("numeroFloat = " + numeroFloat);
+        
 
     }
 }
