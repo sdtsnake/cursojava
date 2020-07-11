61e73c42afc6c70ec48854aa6661a75964e673c1
snake <oscar1940@gmail.com>
2020-07-11 18:00:00 -0500

    tarea del libro con captura de bolieano

--- Variables/src/Variables.java
+++ Variables/src/Variables.java
@@ -8,27 +8,19 @@ import java.util.Scanner;
 public class Variables {
 
     public static void main(String[] args) {
-        //convertir un tipo String a Int
-        var edad = Integer.parseInt("20");
-        //var edad = "20";
-        System.out.println("edad = " + (edad + 1));
-        
-        var valorPi = Double.parseDouble("3.1416");
-        System.out.println("valorPi = " + valorPi);
-        
-          var consola = new Scanner(System.in);
-//        System.out.println("Proporciona tu edad:");
-//        edad = Integer.parseInt(consola.nextLine());
-//        System.out.println("Su edad es :"  + edad);
-        
-        var edadTexto = String.valueOf(10);
-        System.out.println("edadTexto = " + edadTexto);
-        
-        var caracter = "hola".charAt(2);
-        System.out.println("caracter = " + caracter);
-        System.out.println("Proporcia un caracter");
-        caracter = consola.nextLine().charAt(0);
-        System.out.println("caracter = " + caracter);
-        
+        var capturador = new Scanner(System.in);
+        System.out.println("Proporciona el nombre:");
+        var nombre = String.valueOf(capturador.nextLine());
+        System.out.println("Proporciona el id:");
+        var numeroId = Integer.parseInt(capturador.nextLine());
+        System.out.println("Proporciona el precio:");
+        var precio = Double.parseDouble(capturador.nextLine());
+        System.out.println("Envio Gratuito:");
+        boolean envioGratuito = Boolean.parseBoolean(capturador.nextLine());
+
+        System.out.println(nombre + "#" + numeroId);
+        System.out.println("Precio : $" + precio);
+        System.out.println("Envio Gratuito :" + envioGratuito);
+
     }
 }
