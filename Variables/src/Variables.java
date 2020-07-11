a1234448d9fa2df8fb3b8f4e7d2b4bba52dc513b
snake <oscar1940@gmail.com>
2020-07-11 10:43:52 -0500

    manejo primitivo del tipo char

--- Variables/src/Variables.java
+++ Variables/src/Variables.java
@@ -8,15 +8,31 @@ import java.util.Scanner;
 public class Variables {
 
     public static void main(String[] args) {
-        var numeroEntero = 10;
-        System.out.println("numeroEntero = " + numeroEntero);
-        
-        var numeroDouble = 10.0;
-        System.out.println("numeroDouble = " + numeroDouble);
-        
-        var numeroFloat = 10.0F;
-        System.out.println("numeroFloat = " + numeroFloat);
-        
+        char miCaracter = 'a';
+        System.out.println("miCaracter = " + miCaracter);
+        {
+            char varChar = '\u0021';
+            System.out.println("varChar = " + varChar);
+
+            char varCharDecimal = 33;
+            System.out.println("varCharDecimal = " + varCharDecimal);
+
+            char varCharSimbolo = '!';
+            System.out.println("varCharSimbolo = " + varCharSimbolo);
+        }
+        {
+            var varChar = '\u0021';
+            System.out.println("varChar2 = " + varChar);
+            // se castea porque el valor 33 el tipo var infiere que un entero
+            var varCharDecimal = (char)33;
+            System.out.println("varCharDecimal2 = " + varCharDecimal);
+
+            var varCharSimbolo = '!';
+            System.out.println("varCharSimbolo2 = " + varCharSimbolo);
+        }
+        // aquie restorna el valor decimal de los casractes.
+        int variableEnteraSimbolo = 'A';
+        System.out.println("variableEnteraSimbolo = " + variableEnteraSimbolo);
 
     }
 }
