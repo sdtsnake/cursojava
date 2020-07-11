7b372cd883c59a112f47fcf47d205e9204bea895
snake <oscar1940@gmail.com>
2020-07-11 09:51:33 -0500

    datos primitivos de punto flotante

--- Variables/src/Variables.java
+++ Variables/src/Variables.java
@@ -9,27 +9,18 @@ public class Variables {
 
     public static void main(String[] args) {
         /*
-            tipos primitivos enteros byte = 8 bits, short = 16 bits, int = 32 bits, long = 64 bits.
+            tipos primitivos flotante : float y double
          */
-        byte numeroByte = (byte) 129;
-        System.out.println("Valorbyte:" + numeroByte);
-        System.out.println("vakir minino de byte: " + Byte.MIN_VALUE);
-        System.out.println("vakir minino de byte: " + Byte.MAX_VALUE);
-
-        short numeroShort = (short) 32768;
-        System.out.println("numeroShort = " + numeroShort);
-        System.out.println("vakir minino de Short: " + Short.MIN_VALUE);
-        System.out.println("vakir minino de Short: " + Short.MAX_VALUE);
-
-        int numeroInt = (int) 2147483648L;
-        System.out.println("numeroInt = " + numeroInt);
-        System.out.println("valor minino de Short: " + Integer.MIN_VALUE);
-        System.out.println("valor minino de Short: " + Integer.MAX_VALUE);
-
-        long numeroLong = (long)9223372036854775808F;
-        System.out.println("numeroLong = " + numeroLong);
-        System.out.println("valor minino de Long: " + Long.MIN_VALUE);
-        System.out.println("valor minino de Long: " + Long.MAX_VALUE);
+        // se tiene que castear porque 10 es entero
+        float numeroFloat = (float) 10;
+        System.out.println("numeroFloat = " + numeroFloat);
+        System.out.println("valor minimo tipo float:" + Float.MIN_VALUE);
+        System.out.println("valor maximo tipo float:" + Float.MAX_VALUE);
+        // no se castea por tener punto flotante.
+        double numeroDouble = 10.0;
+        System.out.println("numeroDouble = " + numeroDouble);
+        System.out.println("valor minimo tipo double:" + Double.MIN_VALUE);
+        System.out.println("valor maximo tipo double:" + Double.MAX_VALUE);
 
     }
 }
