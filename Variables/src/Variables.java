b199db8aef94b8d2f9196838192a17d2c6e2c1ce
snake <oscar1940@gmail.com>
2020-07-13 10:38:04 -0500

    ejercio del area y del perimetro

--- Variables/src/Variables.java
+++ Variables/src/Variables.java
@@ -8,28 +8,14 @@ import java.util.Scanner;
 public class Variables {
 
     public static void main(String[] args) {
-        int a = 100, b = 2;
-        {
-            var resultado = a + b;
-            System.out.println("La suma de a + b = " + resultado);
-
-            resultado = a - b;
-            System.out.println("La suma de a - b = " + resultado);
-
-            resultado = a * b;
-            System.out.println("La suma de a * b = " + resultado);
-        }
-        var resultado = (float) a / b;
-        System.out.println("La suma de a / b = " + resultado);
-
-        resultado = a % b;
-        System.out.println("Residuo = " + resultado);
-
-        if (a % 2 == 0) {
-            System.out.println("Es numero par");
-        } else {
-            System.out.println("Es numnero impar");
-        }
-
+        var capturador = new Scanner(System.in);
+        System.out.println("Proporciona al alto:");
+        var alto = Double.parseDouble(capturador.nextLine());
+        System.out.println("Proporciona el ancho:");
+        var ancho = Double.parseDouble(capturador.nextLine());
+        var area = alto * ancho;
+        System.out.println("Area: " + area);
+        var perimetro = (alto + ancho) * 2;
+        System.out.println("Perimetro: " + perimetro);
     }
 }
