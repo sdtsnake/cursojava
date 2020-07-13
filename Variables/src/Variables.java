518a003c27553ebdb40010975cd72f302f06a374
snake <oscar1940@gmail.com>
2020-07-13 13:57:54 -0500

    manejo de asignacion basico

--- Variables/src/Variables.java
+++ Variables/src/Variables.java
@@ -8,14 +8,27 @@ import java.util.Scanner;
 public class Variables {
 
     public static void main(String[] args) {
-        var capturador = new Scanner(System.in);
-        System.out.println("Proporciona al alto:");
-        var alto = Double.parseDouble(capturador.nextLine());
-        System.out.println("Proporciona el ancho:");
-        var ancho = Double.parseDouble(capturador.nextLine());
-        var area = alto * ancho;
-        System.out.println("Area: " + area);
-        var perimetro = (alto + ancho) * 2;
-        System.out.println("Perimetro: " + perimetro);
+        int a = 3, b = 2;
+        int c = a + 5 - b;
+        System.out.println("c = " + c);
+        
+        a += 1;
+        System.out.println("a = " + a); 
+        
+        a += 3;
+        System.out.println("a = " + a);
+        
+        a -= 2;
+        System.out.println("a = " + a);
+        
+        a *= 2;
+        System.out.println("a = " + a);
+        
+        a /= 2;
+        System.out.println("a = " + a);
+        
+        a %= 2;
+        System.out.println("a = " + a);
+        
     }
 }
