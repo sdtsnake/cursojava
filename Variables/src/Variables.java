e7b81bfa743526301bc210259ac8ac689a9f1b84
snake <oscar1940@gmail.com>
2020-07-13 19:35:57 -0500

    incementos unitarios

--- Variables/src/Variables.java
+++ Variables/src/Variables.java
@@ -8,27 +8,43 @@ import java.util.Scanner;
 public class Variables {
 
     public static void main(String[] args) {
-        int a = 3, b = 2;
-        int c = a + 5 - b;
+        var a = 3;
+        var b = -a;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        var c = true;
+        var d = !c;
         System.out.println("c = " + c);
-        
-        a += 1;
-        System.out.println("a = " + a); 
-        
-        a += 3;
-        System.out.println("a = " + a);
-        
-        a -= 2;
-        System.out.println("a = " + a);
-        
-        a *= 2;
-        System.out.println("a = " + a);
-        
-        a /= 2;
-        System.out.println("a = " + a);
-        
-        a %= 2;
-        System.out.println("a = " + a);
-        
+        System.out.println("d = " + d);
+
+        {
+            System.out.println("Incrementos");
+            System.out.println("===========");
+            
+            var e = 3;
+            var f = ++e; //pre incremento
+            System.out.println("e = " + e);
+            System.out.println("f = " + f);
+
+            var g = 5;
+            var h = g++; //post incremento
+            System.out.println("g = " + g);
+            System.out.println("h = " + h);
+        }
+        {
+            System.out.println("dectementos");
+            System.out.println("===========");
+            
+            var e = 2;
+            var f = --e; //pre decremento
+            System.out.println("e = " + e);
+            System.out.println("f = " + f);
+
+            var g = 4;
+            var h = g--; //post decremento
+            System.out.println("g = " + g);
+            System.out.println("h = " + h);
+        }
+
     }
 }
