680303112527e2ff3c7850ea8596f3ee5ef01148
snake <oscar1940@gmail.com>
2020-07-22 22:33:22 -0500

    operador ternario

--- Variables/src/Variables.java
+++ Variables/src/Variables.java
@@ -1,6 +1,4 @@
 
-import java.util.Scanner;
-
 /**
  *
  * @author snake
@@ -8,24 +6,11 @@ import java.util.Scanner;
 public class Variables {
 
     public static void main(String[] args) {
-      var a = 11;
-      var valorMinimo =0;
-      var valorMaximo = 10;
-      
-      var resultado = a >= valorMinimo && a <= valorMaximo;
-      if (resultado){
-          System.out.println("Dentro de Rango");   
-      }else{
-          System.out.println("Fuera de Rango");
-      }
-      
-      var vaciones = false;
-      var diaDescanso = true;
-      
-      if (vaciones || diaDescanso){
-          System.out.println("El padre puede asistir al juego de su hijo");
-      }else{
-          System.out.println("El padre esta ocuapado");
-      }
+        var resultado = (1 > 2) ? "verdadero" : "falso" ;
+        System.out.println("resultado = " + resultado);
+        
+        var numero = 9;
+        resultado = (numero % 2 == 0) ? "numero par" : "numero impar";
+        System.out.println("resultado = " + resultado);    
     }
 }
