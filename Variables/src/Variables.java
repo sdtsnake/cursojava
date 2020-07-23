8ae4d0058920c0e7e415323af6a57e8daf971446
snake <oscar1940@gmail.com>
2020-07-22 22:17:30 -0500

    condiconales

--- Variables/src/Variables.java
+++ Variables/src/Variables.java
@@ -8,38 +8,24 @@ import java.util.Scanner;
 public class Variables {
 
     public static void main(String[] args) {
-        var a = 3;
-        var b = 2;
-        
-        var c = (a == b);
-        System.out.println("c = " + c);
-        
-        var d = (a != b);
-        System.out.println("d = " + d);
-        
-        var cadena1 = "Hola";
-        var cadena2 = "Adios";
-        var e = cadena1 == cadena2;
-        System.out.println("e = " + e);
-        
-        var f = cadena1.equals(cadena2);
-        System.out.println("e = " + e);   
-        
-        var g = a >= b;
-        System.out.println("g = " + g);
-        if(a % 2 == 0){
-            System.out.println("Es numero par");
-        }else{
-            System.out.println("Es numero impar");
-        }
-        
-         var edad = 30;
-         var adulto = 18;
-         
-         if (edad > adulto){
-             System.out.println("Es mayor de edad");
-         }else{
-             System.out.println("Es menor de edad");
-         }
+      var a = 11;
+      var valorMinimo =0;
+      var valorMaximo = 10;
+      
+      var resultado = a >= valorMinimo && a <= valorMaximo;
+      if (resultado){
+          System.out.println("Dentro de Rango");   
+      }else{
+          System.out.println("Fuera de Rango");
+      }
+      
+      var vaciones = false;
+      var diaDescanso = true;
+      
+      if (vaciones || diaDescanso){
+          System.out.println("El padre puede asistir al juego de su hijo");
+      }else{
+          System.out.println("El padre esta ocuapado");
+      }
     }
 }
