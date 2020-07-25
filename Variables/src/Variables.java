b24d8a496a7283458dce079195c547d935915419
snake <oscar1940@gmail.com>
2020-07-25 05:26:50 -0500

    prioridad de operadores

--- Variables/src/Variables.java
+++ Variables/src/Variables.java
@@ -8,12 +8,14 @@ import java.util.Scanner;
 public class Variables {
 
     public static void main(String[] args) {
-        var capturador = new Scanner(System.in);
-        System.out.println("Proporciona el numero1:");
-        int numero1 = Integer.parseInt(capturador.nextLine());
-        System.out.println("Proporciona el numero2:");
-        int numero2 = Integer.parseInt(capturador.nextLine());
-        int resultado = (numero1 > numero2) ? numero1 : numero2;
-        System.out.println("El numero mayor es: " + resultado);
+        var x = 5;
+        var y = 10;
+        var z = ++x + y--;
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+        
+        var resultado = (4 + 5) * 6 / 3;
+        System.out.println("resultado = " + resultado);
     }
 }
