dcd45f4558df0d98e4bfb12b4a8c51ba3eeb6c15
snake <oscar1940@gmail.com>
2020-07-27 16:22:35 -0500

    Swithc basico

--- Variables/src/Variables.java
+++ Variables/src/Variables.java
@@ -9,26 +9,25 @@ import java.util.Scanner;
 public class Variables {
 
     public static void main(String[] args) {
-        var estacion = "Estacion desconocida";  
-        var capturador = new Scanner(System.in);
-        System.out.println("Ingrese el mes para saber a que estacion pertenece:");
-        int mes = Integer.parseInt(capturador.nextLine());
-
-        if (mes == 1 || mes == 2 || mes == 12) {
-            estacion = "Invierno";
-        } else if (mes == 3 || mes == 4 || mes == 5) {
-            estacion = "Primavera";
-        } else if (mes == 6 || mes == 7 || mes == 8) {
-            estacion = "Verano";           
-        } else if (mes == 9 || mes == 10 || mes == 11) {
-            estacion = "Otonio";
-        }
-        if (mes >=  1 && mes <= 12)
-        {
-            System.out.println("el mes " + mes + " corresponde a la estacion " + estacion);
-        }else{
-            System.out.println("Mes fuera del rango entre 1 & 12 revise animal");
-        }
+        var numero = 5;
+        String numeroTexto;
         
+        switch(numero){
+            case 1:
+                numeroTexto = "Numero uno";
+                break;
+            case 2:
+                numeroTexto ="Numero dos";
+                break;
+            case 3:
+                numeroTexto = "Numero tres";
+                break;
+            case 4:
+                numeroTexto = "Numero cuatro";
+                break;
+            default:
+                numeroTexto = "Caso desconocido";            
+        }
+        System.out.println("numeroTexto = " + numeroTexto);
     }
 }
