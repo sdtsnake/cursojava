c38869e3937b7e14a04949200e7f95bce98159d5
snake <oscar1940@gmail.com>
2020-07-27 17:33:08 -0500

    while o segun el instructor los juail

--- Variables/src/Variables.java
+++ Variables/src/Variables.java
@@ -8,22 +8,16 @@ import java.util.Scanner;
 public class Variables {
 
     public static void main(String[] args) {
-        var calificaion = "Valor desconocido debe estar entre 0 y 10";
-        var capturador = new Scanner(System.in);
-        System.out.println("Proporciona un valor entre 0 y 10:");
-        double nota = Double.parseDouble(capturador.nextLine());
-
-        if (nota >= 9 && nota <= 10) {
-            calificaion = "A";
-        } else if (nota >= 8 && nota < 9) {
-            calificaion = "B";
-        } else if (nota >= 7 && nota < 8) {
-            calificaion = "C";
-        } else if (nota >= 6 && nota < 7) {
-            calificaion = "D";
-        } else if (nota >= 0 && nota < 6) {
-            calificaion = "F";
-        } 
-        System.out.println("calificaion = " + calificaion);
+        var contador = 0;
+        while(contador < 3){
+            System.out.println("contador = " + contador);
+            contador ++;
+        }
+        contador = 0;
+        do {
+            System.out.println("contador = " + contador); 
+            contador ++;
+        }while(contador < 3);
+        
     }
 }
