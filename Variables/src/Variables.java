d03a477c9d88a01bc24de7bc4430906edf363f7e
snake <oscar1940@gmail.com>
2020-07-27 17:10:59 -0500

    sistema de calificaiones

--- Variables/src/Variables.java
+++ Variables/src/Variables.java
@@ -1,7 +1,6 @@
 
 import java.util.Scanner;
 
-
 /**
  *
  * @author snake
@@ -9,25 +8,22 @@ import java.util.Scanner;
 public class Variables {
 
     public static void main(String[] args) {
-        var numero = 5;
-        String numeroTexto;
-        
-        switch(numero){
-            case 1:
-                numeroTexto = "Numero uno";
-                break;
-            case 2:
-                numeroTexto ="Numero dos";
-                break;
-            case 3:
-                numeroTexto = "Numero tres";
-                break;
-            case 4:
-                numeroTexto = "Numero cuatro";
-                break;
-            default:
-                numeroTexto = "Caso desconocido";            
-        }
-        System.out.println("numeroTexto = " + numeroTexto);
+        var calificaion = "Valor desconocido debe estar entre 0 y 10";
+        var capturador = new Scanner(System.in);
+        System.out.println("Proporciona un valor entre 0 y 10:");
+        double nota = Double.parseDouble(capturador.nextLine());
+
+        if (nota >= 9 && nota <= 10) {
+            calificaion = "A";
+        } else if (nota >= 8 && nota < 9) {
+            calificaion = "B";
+        } else if (nota >= 7 && nota < 8) {
+            calificaion = "C";
+        } else if (nota >= 6 && nota < 7) {
+            calificaion = "D";
+        } else if (nota >= 0 && nota < 6) {
+            calificaion = "F";
+        } 
+        System.out.println("calificaion = " + calificaion);
     }
 }
