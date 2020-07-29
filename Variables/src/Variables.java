ffd75f72ae9d4d1103bd0eb794b7905a94fe16df
snake <oscar1940@gmail.com>
2020-07-29 09:56:44 -0500

    ciclo for

--- Variables/src/Variables.java
+++ Variables/src/Variables.java
@@ -8,16 +8,8 @@ import java.util.Scanner;
 public class Variables {
 
     public static void main(String[] args) {
-        var contador = 0;
-        while(contador < 3){
+        for (int contador = 0; contador < 3; contador++) {
             System.out.println("contador = " + contador);
-            contador ++;
         }
-        contador = 0;
-        do {
-            System.out.println("contador = " + contador); 
-            contador ++;
-        }while(contador < 3);
-        
     }
 }
