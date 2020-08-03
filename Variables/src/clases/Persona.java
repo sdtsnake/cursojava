7ff2f6020820c21d61d9e05181b5b98f86a41ff1
snake <oscar1940@gmail.com>
2020-08-03 15:11:51 -0500

    paso por refrencia

--- Variables/src/clases/Persona.java
+++ Variables/src/clases/Persona.java
@@ -6,8 +6,8 @@ package clases;
  * @author snake
  */
 public class Persona {
-     String nombre;
-     String apellido;
+     public String nombre;
+     public String apellido;
      
      public void desplegarInfomacion() {
          System.out.println("Nombre : " + nombre);
