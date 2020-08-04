70867eb4628f2d085dddd7335d31386dd06c9a97
snake <oscar1940@gmail.com>
2020-08-04 15:32:26 -0500

    contexto estatico

---
+++ ContextoEstatico/src/domain/Persona.java
@@ -0,0 +1,46 @@
+package domain;
+
+/**
+ *
+ * @author snake
+ */
+public class Persona {
+
+    private int idPersona;
+    private String nombre;
+    private static int contatorPesona;
+
+    public Persona(String nombre) {
+        this.nombre = nombre;
+        this.idPersona = Persona.contatorPesona++;
+    }
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public static int getContatorPesona() {
+        return contatorPesona;
+    }
+
+    public static void setContatorPesona(int aContatorPesona) {
+        contatorPesona = aContatorPesona;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "idPersona=" + idPersona + ", nombre=" + nombre + '}';
+    }
+}
