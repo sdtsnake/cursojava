704174d2302e7b0fb71c80d556d9d20cd1498669
snake <oscar1940@gmail.com>
2020-08-07 17:55:42 -0500

    sobre carga de metodos

---
+++ SobreCargaMetodos/src/operaciones/Operaciones.java
@@ -0,0 +1,18 @@
+package operaciones;
+
+/**
+ *
+ * @author snake
+ */
+public class Operaciones {
+    public static int sumar(int a, int b){
+        System.out.println("sumar(int a, int b)");
+        return a + b;
+    }
+    
+    public static double sumar(double a, double b){
+        System.out.println("sumar(double a, double b)");
+        return a + b;
+    }
+    
+}
