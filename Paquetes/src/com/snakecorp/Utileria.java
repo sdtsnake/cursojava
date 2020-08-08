f49926e566e44b988006735fa0407c2ab86f6017
snake <oscar1940@gmail.com>
2020-08-08 16:16:28 -0500

    paquetes

---
+++ Paquetes/src/com/snakecorp/Utileria.java
@@ -0,0 +1,13 @@
+
+package com.snakecorp;
+
+/**
+ *
+ * @author snake
+ */
+public class Utileria {
+    public static void imprimir (String s){
+        System.out.println("s = " + s);
+    }
+    
+}
---
+++ Paquetes/src/test/TestUtileria.java
@@ -0,0 +1,18 @@
+package test;
+//import de los paquetes.
+//import com.snakecorp.Utileria;
+//metodos estaticos
+//import static com.snakecorp.Utileria.imprimir;
+/**
+ *
+ * @author snake
+ */
+public class TestUtileria {
+    public static void main(String[] args) {
+        //Utileria.imprimir("Saludos");
+        //imprimir("Adios");
+        //llamado del metodo por nombre completamente calificado.
+        com.snakecorp.Utileria.imprimir("Nos vemos");
+    }
+    
+}
