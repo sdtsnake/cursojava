b3e044e562e2bb87c4e1e3bdc7eee9897c6cd074
snake <oscar1940@gmail.com>
2020-09-04 21:59:22 -0500

    commite de emergencia por cambio de pc

--- Ventas/src/com/snakecorp/test/VentasTest.java
+++ Ventas/src/com/snakecorp/test/VentasTest.java
@@ -11,10 +11,17 @@ public class VentasTest {
     public static void main(String[] args) {
         Producto producto1 = new Producto("Camisa", 50.00);
         Producto producto2 = new Producto("Pantalon", 100.00);
-        
+        Producto producto3 = new Producto("medias", 10.00);
+
         Orden orden1 = new Orden();
         orden1.agregarProducto(producto1);
         orden1.agregarProducto(producto2);
         orden1.mostrarOrden();
+
+        Orden orden2 = new Orden();
+        orden2.agregarProducto(producto1);
+        orden2.agregarProducto(producto2);
+        orden2.mostrarOrden();
+
     }
 }
